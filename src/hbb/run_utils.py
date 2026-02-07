1ed43b7f33b4f062ffeeb20e7b8f0cbb124457e3
Kasia Krzyzanska <kk829@cornell.edu>
2026-02-07 17:12:12 -0600

    Fixed git repo link

--- src/hbb/run_utils.py
+++ src/hbb/run_utils.py
@@ -59,7 +59,7 @@ def check_branch(git_branch: str, allow_diff_local_repo: bool = False):
     """Check that specified git branch exists in the repo, and local repo is up-to-date"""
     assert not bool(
         os.system(
-            f'git ls-remote --exit-code --heads "https://github.com/DAZSLE/hbb-run3" "{git_branch}"'
+            f'git ls-remote --exit-code --heads "https://github.com/kk428/hbb-run3" "{git_branch}"'
         )
     ), f"Branch {git_branch} does not exist"
 
