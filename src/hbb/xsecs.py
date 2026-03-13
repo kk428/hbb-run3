fbbedfcb17b44a05bd567597df419c0898c092b9
Kasia Krzyzanska <kk829@cornell.edu>
2026-03-13 17:07:01 -0500

    Added cross section for VBS

--- src/hbb/xsecs.py
+++ src/hbb/xsecs.py
@@ -16,6 +16,9 @@ BR_HCC = 2.860e-02
 
 xsecs = {}
 
+# VBS Signal
+xsecs["VBSWLWLToJets_Minus"] = 0.1208
+
 # GJets: Obtained from XSDB
 xsecs["GJ_PTG-20to100"] = 195300
 xsecs["GJ_PTG-100to200"] = 1396
